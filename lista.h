e3c6503f5d3181f5281f71d36853e4d016e76738
computeiro <computeiro@debian>
2015-06-11 11:11:55 -0300

    Funcoes do Lucas

---
+++ lista.h
@@ -0,0 +1,90 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+#define ListaEstaVazia    - 29
+#define ArgumentoInvalido -  1
+#define False                0
+#define True                 1
+#define Sucesso              1
+
+typedef int (* FuncaoComparacao)(const void *, const void *);
+
+typedef struct NoLista {
+    char * Info;
+    struct NoLista * Proximo;
+} NoLista;
+
+typedef struct{
+    NoLista * Primeiro;
+} List;
+
+/**
+ * @brief Cria uma Lista Ligada
+ *
+ * @return List * Ponteiro para a Lista
+ */
+List * CriaLista();
+
+/**
+ * @brief Libera toda a memória da Lista.
+ *
+ * @param List * Ponteiro para a Lista
+ *
+ * @return Retorna código de erro
+ */
+int DestroiLista(List * );
+
+/**
+ * @brief Verifica se a lista está vazia
+ *
+ * @param List * Ponteiro para a List
+ *
+ * @return Retorna o resultado (True/False)
+ */
+int ListaVazia(List * );
+
+/**
+ * @brief Insere um elemento
+ *
+ * @param List * Ponteiro para a Lista
+ * @param void * Ponteiro para o Elemento
+ *
+ * @return Retorna código de erro
+ */
+int InserirLista(List *, char * );
+
+/**
+ * @brief Remove todos os elementos específicos
+ *
+ * @param List * Ponteiro para a Lista
+ * @param FuncaoComparacao Funcao especifica para o Elemento
+ * @param const void * Especifidade
+ *
+ * @return Retorna quantidade de elementos removidos
+ */
+int RemoverLista(List *, FuncaoComparacao, const void * );
+
+/**
+ * @brief Pesquisa na lista
+ *
+ * @param List * Ponteiro para a Lista
+ * @param FuncaoComparacao Funcao específica para o Elemento
+ * @param const void * Especifidade
+ *
+ * @return Retorna código de erro
+ */
+int BuscarLista (List *, FuncaoComparacao, const void * );
+
+/**
+ * @brief Ordena (QuickSort) a Lista
+ *
+ * @param NoLista ** Ponteiro de Ponteiro para o Primeiro Elemento
+ * @param FuncaoComparacao Funcao específica para o tipo de Elemento
+ *
+ * @return Retorna código de erro
+ */
+void Ordena(NoLista **, FuncaoComparacao);
+
+int ComparaNomeNasListas( NoLista *, char *);
+
+#endif // LISTA_H
